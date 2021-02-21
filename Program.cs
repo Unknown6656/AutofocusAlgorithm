e799cf81334782d4511c8e18c90a4908cb02ee72
unknown6656 <unknown84@gmx.de>
2021-02-21 19:45:16 +0100

    switched to top-level statements

--- Program.cs
+++ Program.cs
@@ -1,46 +1,36 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.IO;
 using System;
 
-namespace AutofocusAlgorithm
+using AutofocusAlgorithm;
+
+
+List<(Scene, string)> scenes = new List<(Scene, string)>();
+
+foreach (string dir in Directory.GetDirectories(Directory.GetCurrentDirectory()))
 {
-    using static Math;
-    using static Util;
+    int count = 0;
+    Scene s = new Scene(128, 128);
+    var files = (from f in Directory.GetFiles(dir)
+                 orderby f ascending
+                 where f.match(@"\.png", out _)
+                 select new
+                 {
+                     Image = Image.FromFile(f) as Bitmap,
+                     Index = count++
+                 }).ToArray();
 
+    foreach (var f in files)
+        s.Add(new SceneLayer(1f - f.Index / (float)count, f.Image));
 
-    public static class Program
-    {
-        public static int Main(string[] argv)
-        {
-            List<(Scene, string)> scenes = new List<(Scene, string)>();
-
-            foreach (string dir in Directory.GetDirectories(Directory.GetCurrentDirectory()))
-            {
-                int cnt = 0;
-                Scene s = new Scene(128, 128);
-                var files = from f in Directory.GetFiles(dir)
-                            orderby f ascending
-                            where f.match(@"\.png", out _)
-                            select (Image.FromFile(f) as Bitmap, cnt++);
-
-                foreach ((Bitmap, int) f in files)
-                    s.Add(new SceneLayer(1f - f.Item2 / (float)cnt, f.Item1));
-
-                scenes.Add((s, dir));
-            }
-            
-            new LiveSceneViewer(scenes.ToArray()).ShowDialog();
-
-            //Console.WriteLine("Press any key to exit ...");
-            //Console.ReadKey(true);
-
-            return 0;
-        }
-
-       
-    }
+    scenes.Add((s, dir));
 }
+
+new LiveSceneViewer(scenes.ToArray()).ShowDialog();
+
+//Console.WriteLine("Press any key to exit ...");
+//Console.ReadKey(true);
+
+return 0;
